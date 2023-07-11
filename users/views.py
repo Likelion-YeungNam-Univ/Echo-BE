02229f95053be9ce1967e73adfc3ab5bb536a78d
arty314 <yu21iarty@gmail.com>
2023-07-11 21:25:19 +0900

    LOGINandProfile

--- users/views.py
+++ users/views.py
@@ -9,6 +9,9 @@ class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
 
+
+    
+
 class LoginView(generics.GenericAPIView):
     serializer_class = LoginSerializer
 
@@ -18,6 +21,20 @@ class LoginView(generics.GenericAPIView):
         token = serializer.validated_data
         return Response({"token": token.key}, status=status.HTTP_200_OK)
 
+# profileView
+# class ProfileView(generics.GenericAPIView):
+#     serializer_class = ProfileSerializer
+
+#     def update(self, request):
+#         serializer = self.get_serializer(data = request.data)
+#         if serializer.is_valid(raise_exception=True):
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_200_OK)
+#         else :
+#             return Response(status=status.HTTP_400_BAD_REQUEST)
+
 class ProfileView(generics.RetrieveAPIView):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
+    lookup_field = 'pk'
+
